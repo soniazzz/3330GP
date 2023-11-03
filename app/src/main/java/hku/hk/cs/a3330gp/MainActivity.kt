a95d032b763c2d71a84c9eee3fb46026ec468588
soniazzz <soniaz05@connect.hku.hk>
2023-11-04 00:24:11 +0800

    Add new project

---
+++ app/src/main/java/hku/hk/cs/a3330gp/MainActivity.kt
@@ -0,0 +1,11 @@
+package hku.hk.cs.a3330gp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
